1720f4d469950076263add514b0bc0b66f54edb3
shroommm <khacnguyenquan@gmail.com>
2023-11-12 20:09:08 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Page() {
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
-            , brought to you by Vercel.
+            , brought to you by Vercel (and me!!).
           </p>
           <div
             className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"
